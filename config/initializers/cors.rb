461ac7ceaff1f06d83adc09cc2d94482261b55b3
Timur~ <galy.tim@gmail.com>
2023-07-31 23:39:54 +0300

    add expose Authorization in cors file

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -6,5 +6,6 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
     resource "*",
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      expose:  %w[Authorization Uid]
   end
 end
