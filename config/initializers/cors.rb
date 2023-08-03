0d72111a4d416abef6de8fae267edb66daa81b95
Timur~ <galy.tim@gmail.com>
2023-08-03 13:18:34 +0300

    set cors for develop

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,7 +1,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins "https://secret-santa-sevsu.vercel.app"
+    origins "*" #"https://secret-santa-sevsu.vercel.app"
 
     resource "*",
       headers: :any,
