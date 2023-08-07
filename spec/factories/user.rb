3bbc24f692ea191fa066fc599157ad8909a358a4
Timur~ <galy.tim@gmail.com>
2023-08-07 22:53:03 +0300

    add wishlist factory

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -3,8 +3,9 @@ FactoryBot.define do
   factory :user do
     name { FFaker::Name.name }
     email { FFaker::Internet.email }
+    sex {1}
     password { 'password' }
     jti { SecureRandom.uuid }
-    
+   
   end
 end
