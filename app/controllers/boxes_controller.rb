43439899512cf6d0a23aed50bf7a482154435d3e
Timur~ <galy.tim@gmail.com>
2023-08-23 18:18:52 +0300

    fix strong params name

--- app/controllers/boxes_controller.rb
+++ app/controllers/boxes_controller.rb
@@ -178,6 +178,6 @@ class BoxesController < ApplicationController
   end
 
   def box_params
-    params.require(:box).permit(:nameBox, :dateFrom, :dateTo, :priceFrom, :priceTo, :place, :description, :image)
+    params.require(:box).permit(:name, :dateFrom, :dateTo, :priceFrom, :priceTo, :place, :description, :image)
   end
 end
