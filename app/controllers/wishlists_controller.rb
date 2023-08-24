0b7d963e115531d06e9f852433922f3c97df9669
Timur~ <galy.tim@gmail.com>
2023-08-24 09:16:21 +0300

    refactor #filtred_index in Wishlist_controller

--- app/controllers/wishlists_controller.rb
+++ app/controllers/wishlists_controller.rb
@@ -9,34 +9,10 @@ class WishlistsController < ApplicationController
       filters_data = filters[:filters]
       user = User.find(params[:user_id])
     
-      wishlists = user.wishlists
-    
-      if filters_data.blank? || filters_data.none? { |filter| filter[:field].present? && filter[:value].present? && Wishlist.column_names.include?(filter[:field]) }
-        total_count = wishlists.count
-        wishlists_data = wishlists.page(page).per(size).map do |wishlist|
-          wishlist_data = wishlist.attributes.except('created_at', 'updated_at')
-        end
-        render json: { items: wishlists_data, totalCount: total_count }
-      else
-        result_data = []
-        total_count = 0
-    
-        filters_data.each do |filter|
-          field = filter[:field]
-          search_value = filter[:value]
-          next unless field.present? && search_value.present? && Wishlist.column_names.include?(field)
-    
-          wishlist_query = wishlists.where("#{field} ILIKE ?", "%#{search_value}%")
-          total_count += wishlist_query.count
-          wishlist_data = wishlist_query.page(page).per(size).map do |wishlist|
-            wishlist.attributes.except('created_at', 'updated_at')
-          end
-          result_data.concat(wishlist_data)
-        end
-    
-        render json: { items: result_data, totalCount: total_count }
-      end
+      result = Wishlist.filter_and_present(filters_data, user, page, size)
+      render json: result, status: :ok
     end
+    
     
     
 
--- app/models/wishlist.rb
+++ app/models/wishlist.rb
@@ -2,6 +2,30 @@ class Wishlist < ApplicationRecord
     belongs_to :user
     validates :description, presence: true
 
-    
+    scope :filtered_by_field, ->(field, search_value) {
+      where("#{field} ILIKE ?", "%#{search_value}%") if column_names.include?(field)
+    }
+  
+    def self.filter_and_present(filters_data, user, page, size)
+      wishlists = user.wishlists
+  
+      if filters_data.present?
+        filters_data.each do |filter|
+          field = filter[:field]
+          search_value = filter[:value]
+          next unless field.present? && search_value.present?
+  
+          wishlists = wishlists.filtered_by_field(field, search_value)
+        end
+      end
+  
+      total_count = wishlists.count
+      wishlists = wishlists.page(page).per(size)
+      wishlists_data = wishlists.map do |wishlist|
+        wishlist.attributes.except('created_at', 'updated_at',"user_id")
+      end
+  
+      { items: wishlists_data, totalCount: total_count }
+    end
   end
   
