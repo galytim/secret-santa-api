23d06247b641ce6664923cf814f9676ad9a577ef
Timur~ <galy.tim@gmail.com>
2023-09-19 21:09:31 +0300

    test deleting field

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_08_24_080300) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_19_180734) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "boxes", force: :cascade do |t|
     t.string "name"
-    t.date "DateTo"
     t.integer "priceFrom"
     t.integer "priceTo"
     t.string "place"
