cd6e3ce83d9944f063b7150b9fd421ac5e572f9d
Timur~ <galy.tim@gmail.com>
2023-09-19 21:18:40 +0300

    add field again

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,12 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_09_19_180734) do
+ActiveRecord::Schema[7.0].define(version: 2023_09_19_181630) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "boxes", force: :cascade do |t|
     t.string "name"
-    t.date "dateTo"
     t.integer "priceFrom"
     t.integer "priceTo"
     t.string "place"
@@ -26,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_09_19_180734) do
     t.bigint "admin_id"
     t.string "description"
     t.boolean "invitable", default: true
+    t.date "dateTo"
   end
 
   create_table "boxes_users", id: false, force: :cascade do |t|
