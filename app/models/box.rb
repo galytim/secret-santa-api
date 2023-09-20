9dea3c6f78c70f1e3df4c1b7840a4add92684fd4
Timur~ <galy.tim@gmail.com>
2023-09-20 21:13:38 +0300

    add image in box

--- app/models/box.rb
+++ app/models/box.rb
@@ -1,8 +1,9 @@
 class Box < ApplicationRecord
+    include ImageUploader::Attachment(:image)
     belongs_to :admin, class_name: "User", foreign_key: "admin_id"
     has_and_belongs_to_many :participants, class_name: "User"
     has_many :pairs, dependent: :destroy
-
+    
     def second_registered_user
         second_user = participants.order("boxes_users.created_at ASC").second
         second_user if second_user.present?
