faa9584e55716b83eeb1b4667ebad6761a58af39
Timur~ <galy.tim@gmail.com>
2023-09-21 19:29:56 +0300

    fix production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -12,8 +12,8 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address: 'smtp.gmail.com',
     port: 587,
-    user_name: Rails.application.credentials.stripe[:gmail][:email], 
-    password: Rails.application.credentials.stripe[:gmail][:password], 
+    user_name: Rails.application.credentials.gmail[:email], 
+    password: Rails.application.credentials.gmail[:password], 
     authentication: 'plain',
     enable_starttls_auto: true
   }
